eb127c8908cdd7c47790de48e10b3bf48a8d7d8e
bguss03 <150691439+bguss03@users.noreply.github.com>
2025-08-06 23:18:49 +0700

    update again

--- src/Components/Layout/Tentang.tsx
+++ src/Components/Layout/Tentang.tsx
@@ -6,7 +6,7 @@ export default function Tentang() {
           About
         </h1>
         <div>
-          <p className="text-2xl text-center font-mono py-3">
+          <p className="text-xl text-center font-mono py-3">
             Hi! My name is Ananda Bagus Shevchenko (usually called Bagus). I am a Front End Developer and Full Stack Enthusiast.
             With a background in Software Engineering from SMK, I am currently continuing my education as a student at Semarang University. I am eager to apply my skills and contribute professionally to the world of software development.
           </p>
